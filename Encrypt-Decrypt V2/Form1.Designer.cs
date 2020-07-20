ec6c392e6242a3b686e2e126ef1a9fb9739456d8
Levente <Levente@DESKTOP-JPGKIQC>
2020-07-20 23:04:26 +0200

    2020.07.20.

---
+++ Encrypt-Decrypt V2/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Encrypt_Decrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Input_textBox = new System.Windows.Forms.TextBox();
+            this.Output_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.button = new System.Windows.Forms.Button();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Input_textBox
+            // 
+            this.Input_textBox.Location = new System.Drawing.Point(12, 61);
+            this.Input_textBox.Multiline = true;
+            this.Input_textBox.Name = "Input_textBox";
+            this.Input_textBox.Size = new System.Drawing.Size(295, 204);
+            this.Input_textBox.TabIndex = 0;
+            // 
+            // Output_textBox
+            // 
+            this.Output_textBox.Location = new System.Drawing.Point(322, 61);
+            this.Output_textBox.Multiline = true;
+            this.Output_textBox.Name = "Output_textBox";
+            this.Output_textBox.Size = new System.Drawing.Size(295, 204);
+            this.Output_textBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "INPUT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "OUTPUT";
+            // 
+            // comboBox
+            // 
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "ENCRYPT",
+            "DECRYPT"});
+            this.comboBox.Location = new System.Drawing.Point(12, 13);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 21);
+            this.comboBox.TabIndex = 4;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(193, 12);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 5;
+            this.button.Text = "Go";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(139, 14);
+            this.numericUpDown.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 277);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Output_textBox);
+            this.Controls.Add(this.Input_textBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Encrypt - Decrypt V2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input_textBox;
+        private System.Windows.Forms.TextBox Output_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+    }
+}
+
---
+++ Encrypt-Decrypt V2/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encrypt_Decrypt
+{
+    public partial class Form1 : Form
+    {
+        readonly Dictionary<char, int> dict = new Dictionary<char, int>()
+        {
+            {' ', 44911},
+            {'q', 14852},
+            {'w', 30763},
+            {'e', 59349},
+            {'r', 40809},
+            {'t', 37724},
+            {'z', 16161},
+            {'u', 25306},
+            {'i', 59720},
+            {'o', 38258},
+            {'p', 28161},
+            {'ő', 48796},
+            {'ú', 11733},
+            {'ű', 3701},
+            {'a', 61800},
+            {'s', 41459},
+            {'d', 28647},
+            {'f', 53815},
+            {'g', 48989},
+            {'h', 35698},
+            {'j', 64850},
+            {'k', 2280},
+            {'l', 57014},
+            {'é', 33687},
+            {'á', 62093},
+            {'í', 39156},
+            {'y', 35572},
+            {'x', 33102},
+            {'c', 42990},
+            {'v', 26608},
+            {'b', 57599},
+            {'n', 33086},
+            {'m', 40486},
+            {'Q', 14895},
+            {'W', 38420},
+            {'E', 47969},
+            {'R', 35172},
+            {'T', 11616},
+            {'Z', 44556},
+            {'U', 41087},
+            {'I', 37392},
+            {'O', 60322},
+            {'P', 19547},
+            {'Ő', 56907},
+            {'Ú', 49856},
+            {'Ű', 21364},
+            {'A', 61796},
+            {'S', 46411},
+            {'D', 6920},
+            {'F', 47810},
+            {'G', 17877},
+            {'H', 11233},
+            {'J', 2980},
+            {'K', 45797},
+            {'L', 55589},
+            {'É', 42632},
+            {'Á', 23370},
+            {'Í', 62609},
+            {'Y', 62789},
+            {'X', 1653},
+            {'C', 2152},
+            {'V', 48591},
+            {'B', 47849},
+            {'N', 33113},
+            {'M', 46191},
+            {'0', 60607},
+            {'1', 8311},
+            {'2', 62370},
+            {'3', 30813},
+            {'4', 13831},
+            {'5', 6288},
+            {'6', 17031},
+            {'7', 9259},
+            {'8', 7903},
+            {'9', 63839},
+            {',', 4528},
+            {'.', 48266},
+            {'/', 49363},
+            {'*', 40729},
+            {'-', 42893},
+            {'!', 54694},
+            {'?', 32410},
+            {'=', 60712},
+            {'(', 8731},
+            {')', 46367},
+            {'%', 32788},
+            {'\n', 60221},
+            {'\r', 24838},
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Output_textBox.Text = string.Empty;
+            if (Input_textBox.Text != string.Empty)
+            {
+                List<char> charList = Input_textBox.Text.ToCharArray().ToList();
+                int addition = (int)numericUpDown.Value;
+                if (comboBox.SelectedIndex == 0)
+                {
+                    charList.ForEach(c => { Output_textBox.Text += Encrypt(c, addition); });
+                }
+                else
+                {
+                    charList.ForEach(c => { Output_textBox.Text += Decrypt(c, addition); });
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Please write something!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        public string Encrypt(char c, int addition)
+        {
+            int number = 35; //35 is hashtag, only happens if the char is not in the dict
+            foreach (var item in dict)
+            {
+                if (item.Key == c)
+                {
+                    number = item.Value + addition;
+                }
+            }
+
+            return ((char)number).ToString();
+        }
+
+        public string Decrypt(char c, int addition)
+        {
+            foreach (var item in dict)
+            {
+                if (((int)c-addition) == item.Value)
+                {
+                    return item.Key.ToString();
+                }
+            }
+
+            return "-UNKNOWN-";
+        }
+    }
+}
